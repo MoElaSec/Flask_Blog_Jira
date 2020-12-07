294431d4d4ff89e0c634403ba5488cbc6bba6a73
ShifraSec <48570596+MoElaSec@users.noreply.github.com>
2020-12-07 16:27:42 +0400

    Add files via upload

---
+++ Flask/flaskblog/posts/routes.py
@@ -0,0 +1,69 @@
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from flaskblog import db
+from flaskblog.models import Post
+from flaskblog.posts.forms import PostForm
+
+
+
+posts = Blueprint("posts", __name__)
+
+
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+    
+    
+ #using the id we can specify the desired post(int: to cast it to an int make-sure)
+@posts.route("/post/<int:post_id>") # we get the id from the home.html --> post_id=post.id
+def post(post_id):
+    post = Post.query.get_or_404(post_id) #give me a post or return 404
+    return render_template('post.html', title=post.title,
+                            post=post, legend="New Post")
+
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403) #HTTP respond for wrong rout
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data #change the legend ad also fill-in data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post') #no reason to creat a whole tamblet just to update hence we use Legend
+
+
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
+
+
+ 
